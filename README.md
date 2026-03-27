6d647dffdc61def943ac7c5eb1e37ac2c01dfabd
fatalus <der_fatal_@outlook.com>
2026-03-27 13:40:02 +0100

    chore: stuff for server

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # dotfiles
 
 ```bash
+git clone git@github.com:fatalus/dotfiles.git "$HOME/.config/dotfiles"
+```
+
+```bash
 # load exports first, then aliases
 source "$HOME/.config/dotfiles/prompt.sh"
 source "$HOME/.config/dotfiles/exports.sh"
--- prompt.sh
+++ prompt.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# Just the standard bash prompt...
-PS1='\[\e[92m\]\u@\h\[\e[0m\]:\[\e[34m\]\w\[\e[0m\]\\$ '
-
-# Root prompt for Servers:
-# PS1='\[\e[91m\]\u\[\e[0m\]@\[\e[96m\]\h\[\e[0m\]:\[\e[33m\]\w\[\e[0m\]\$ ' 
+if [[ "$HOME" == "/root" ]]; then
+    # Root prompt for servers
+    PS1='\[\e[91m\]\u\[\e[0m\]@\[\e[96m\]\h\[\e[0m\]:\[\e[33m\]\w\[\e[0m\]\$ '
+else
+    # more or less normal bash prompt
+    PS1='\[\e[92m\]\u@\h\[\e[0m\]:\[\e[34m\]\w\[\e[0m\]\\$ '
+fi
